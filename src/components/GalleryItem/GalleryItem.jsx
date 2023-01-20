d71b687adb9299185a6f401ccffb49b72b056511
David Trelles <davidstrelles@gmail.com>
2023-01-20 06:10:05 -0600

    added css to remove bullets

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,7 @@
 function GalleryItem({image}){
     return(
         <>
-        <li ><img src={image.path} style={{width: 200 + "px", height: 200 + "px"}}></img></li>
+        <li ><img src={image.path} style={{width: 100 + "px", height: 100 + "px"}}></img></li>
         </>
 
 
